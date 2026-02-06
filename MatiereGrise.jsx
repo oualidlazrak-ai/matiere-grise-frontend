f71ba4fd1b9c3fbb51430157d6b2bf20d8037aa6
oualidlazrak-ai <oualid.lazrak@ensp-arles.fr>
2026-02-06 16:37:13 +0100

    Update API URL tunnel

--- MatiereGrise.jsx
+++ MatiereGrise.jsx
@@ -21,7 +21,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 // Configuration - À CHANGER selon ton tunnel
-const API_URL = 'notepad C:\Users\ouali\matiere-grise\matiere-grise-frontend\MatiereGrise.jsx';
+const API_URL = 'https://accuracy-disclaimers-accompanying-dam.trycloudflare.com';
 
 export default function MatiereGriseV2() {
   // Auth state
